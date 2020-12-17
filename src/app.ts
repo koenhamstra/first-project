96da0b3118fcb7e01f27160ea03a5723512a9cf2
koenhamstra <70519997+koenhamstra@users.noreply.github.com>
2020-12-17 10:57:21 +0100

    mario-level

--- src/app.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Start the game whenever the entire DOM is loaded
- */
-let init = () =>
-  new Game(document.getElementById("canvas") as HTMLCanvasElement);
-
-// Add EventListener to load the game whenever the browser is ready
-window.addEventListener("load", init);
