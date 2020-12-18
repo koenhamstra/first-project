8b2026af3b37d3d9d8b81d8b83c640a6f2d77ff3
amjadhwidy <70522055+amjadhwidy@users.noreply.github.com>
2020-12-18 10:34:04 +0100

    Update Game.ts

--- src/Game.ts
+++ src/Game.ts
@@ -2,13 +2,8 @@ class Game {
 
     private  canvas: HTMLCanvasElement;
     private  ctx: CanvasRenderingContext2D;
-
-    private array : HTMLImageElement[];
-    private leftArray : HTMLImageElement[];
-    private keyboard : KeyboardListener;
-    private index : number;
-    private xpos :number;
-    private ypos :number;
+    private index  :number ;
+    private player: Player;
     private enemy :HTMLImageElement;
 
 
@@ -16,47 +11,37 @@ class Game {
 
     public constructor(canvasId: HTMLCanvasElement) {
         // Construct all of the canvas
+        this.index=0;
         this.canvas = canvasId;
         // Resize the canvas to fit the entire window
         this.canvas.width = window.innerWidth;
         this.canvas.height = window.innerHeight;
 
+
+        //player
+        this.player=new Player(canvasId);
+
         // Set the context of the canvas
         this.ctx = this.canvas.getContext('2d');
         document.body.style.backgroundImage = "url('src/moving/back.png')";
         document.body.style.backgroundSize = "cover";
-        requestAnimationFrame(this.loop);
-
-        this.xpos = this.canvas.width/10
-        this.ypos = this.canvas.height* 8.6/10;
-
-        this.keyboard = new KeyboardListener;
-
-        this.array = [this.loadNewImage("src/moving/PlayerRight/walk 1.png") , this.loadNewImage("src/moving/PlayerRight/walk 2.png"), this.loadNewImage("src/moving/PlayerRight/walk 3.png") ,this.loadNewImage("src/moving/PlayerRight/walk 4.png"), this.loadNewImage("src/moving/PlayerRight/walk 5.png"), this.loadNewImage("src/moving/PlayerRight/walk 6.png"), this.loadNewImage("src/moving/PlayerRight/walk 7.png")];
-        this.leftArray = [this.loadNewImage("src/moving/PlayerLeft/walk 1.png") , this.loadNewImage("src/moving/PlayerLeft/walk 2.png"), this.loadNewImage("src/moving/PlayerLeft/walk 3.png") ,this.loadNewImage("src/moving/PlayerLeft/walk 4.png"), this.loadNewImage("src/moving/PlayerLeft/walk 5.png"), this.loadNewImage("src/moving/PlayerLeft/walk 6.png"), this.loadNewImage("src/moving/PlayerLeft/walk 7.png")];
-        this.index = 0;
-        this.enemy = this.loadNewImage("src/moving/enemy/shoot 2.png");
+        requestAnimationFrame(this.loop); 
     }
 
 
-    private drawing = (image : HTMLImageElement) : void => {
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    private draw = (image : HTMLImageElement) : void => {
         this.ctx.drawImage(this.enemy,this.canvas.width/2, this.canvas.height/2);
-        this.ctx.drawImage(image,this.xpos , this.ypos);
     }
 
 
     public loop = () => {
-        // this.drawing(this.loadNewImage("src/moving/enemy/shoot 1.png"))
         if (this.index > 29) {
             this.index = 0;
         }
-
-        this.moveRight();
-        
-        this.moveLeft();
+        this.player.moveRight();
+        this.player.moveLeft();
+        this.player.jump()
     
-        this.jump();
 
         requestAnimationFrame(this.loop)
     }
@@ -69,97 +54,9 @@ class Game {
         return img;
     }
 
-    private jump() {
-        if (this.keyboard.isKeyDown(32) === true) {
-            this.ypos = this.ypos - 20;
-            // if (this.ypos <= this.canvas.height*7/10) {
-            //     this.ypos =  this.canvas.height*7/10;
-            // }
-        }
 
-        if (this.keyboard.isKeyDown(32) === false) {
-            this.ypos = this.ypos + 20;
-            if (this.ypos > this.canvas.height * 8.6 / 10) {
-                this.ypos = this.canvas.height * 8.6 / 10;
-            }
-        }
-    }
 
-    private moveLeft() {
-        if (this.keyboard.isKeyDown(37) === true) {
-            this.index++;
-            if (this.index === 4) {
-                this.drawing(this.leftArray[0]);
-            }
 
-            if (this.index === 8) {
-                this.drawing(this.leftArray[1]);
-            }
 
-            if (this.index === 12) {
-                this.drawing(this.leftArray[2]);
-            }
 
-            if (this.index === 16) {
-                this.drawing(this.leftArray[3]);
-            }
-
-            if (this.index === 20) {
-                this.drawing(this.leftArray[4]);
-            }
-
-            if (this.index === 24) {
-                this.drawing(this.leftArray[5]);
-            }
-            if (this.index === 28) {
-                this.drawing(this.leftArray[6]);
-                this.index = 0;
-            }
-
-            if (this.xpos === 0) {
-                this.xpos = this.canvas.width;
-            }
-
-            this.xpos = this.xpos - 4;
-        }
-    }
-
-    private moveRight() {
-        if (this.keyboard.isKeyDown(39) === true) {
-            this.index++;
-            if (this.index === 4) {
-                this.drawing(this.array[0]);
-            }
-
-            if (this.index === 8) {
-                this.drawing(this.array[1]);
-            }
-
-            if (this.index === 12) {
-                this.drawing(this.array[2]);
-            }
-
-            if (this.index === 16) {
-                this.drawing(this.array[3]);
-            }
-
-            if (this.index === 20) {
-                this.drawing(this.array[4]);
-            }
-
-            if (this.index === 24) {
-                this.drawing(this.array[5]);
-            }
-            if (this.index === 28) {
-                this.drawing(this.array[6]);
-                this.index = 0;
-            }
-
-            if (this.xpos === this.canvas.width * 5 / 5) {
-                this.xpos = 0;
-            }
-
-            this.xpos = this.xpos + 4;
-        }
-    }
 }
