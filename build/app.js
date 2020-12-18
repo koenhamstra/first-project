9e1cde827d38fee6053fe532cd12769b3a8be531
amjadhwidy <70522055+amjadhwidy@users.noreply.github.com>
2020-12-18 10:15:21 +0100

    Update app.js

--- build/app.js
+++ build/app.js
@@ -2,81 +2,16 @@ class Game {
     constructor(canvasId) {
         this.drawing = (image) => {
             this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            this.ctx.drawImage(this.enemy, this.canvas.width / 2, this.canvas.height / 2);
             this.ctx.drawImage(image, this.xpos, this.ypos);
         };
         this.loop = () => {
-            this.drawing(this.loadNewImage("src/moving/enemy/shoot 1.png"));
             if (this.index > 29) {
                 this.index = 0;
             }
-            if (this.keyboard.isKeyDown(39) === true) {
-                this.index++;
-                if (this.index === 4) {
-                    this.drawing(this.array[0]);
-                }
-                if (this.index === 8) {
-                    this.drawing(this.array[1]);
-                }
-                if (this.index === 12) {
-                    this.drawing(this.array[2]);
-                }
-                if (this.index === 16) {
-                    this.drawing(this.array[3]);
-                }
-                if (this.index === 20) {
-                    this.drawing(this.array[4]);
-                }
-                if (this.index === 24) {
-                    this.drawing(this.array[5]);
-                }
-                if (this.index === 28) {
-                    this.drawing(this.array[6]);
-                    this.index = 0;
-                }
-                if (this.xpos === this.canvas.width * 5 / 5) {
-                    this.xpos = 0;
-                }
-                this.xpos = this.xpos + 4;
-            }
-            if (this.keyboard.isKeyDown(32) === true) {
-                this.ypos = this.ypos - 20;
-            }
-            if (this.keyboard.isKeyDown(37) === true) {
-                this.index++;
-                if (this.index === 4) {
-                    this.drawing(this.leftArray[0]);
-                }
-                if (this.index === 8) {
-                    this.drawing(this.leftArray[1]);
-                }
-                if (this.index === 12) {
-                    this.drawing(this.leftArray[2]);
-                }
-                if (this.index === 16) {
-                    this.drawing(this.leftArray[3]);
-                }
-                if (this.index === 20) {
-                    this.drawing(this.leftArray[4]);
-                }
-                if (this.index === 24) {
-                    this.drawing(this.leftArray[5]);
-                }
-                if (this.index === 28) {
-                    this.drawing(this.leftArray[6]);
-                    this.index = 0;
-                }
-                if (this.xpos === 0) {
-                    this.xpos = this.canvas.width;
-                }
-                this.xpos = this.xpos - 4;
-            }
-            if (this.keyboard.isKeyDown(32) === false) {
-                this.ypos = this.ypos + 20;
-                if (this.ypos > this.canvas.height * 8.6 / 10) {
-                    this.ypos = this.canvas.height * 8.6 / 10;
-                }
-            }
-            console.log(this.index);
+            this.moveRight();
+            this.moveLeft();
+            this.jump();
             requestAnimationFrame(this.loop);
         };
         this.loadNewImage = (source) => {
@@ -97,7 +32,80 @@ class Game {
         this.array = [this.loadNewImage("src/moving/PlayerRight/walk 1.png"), this.loadNewImage("src/moving/PlayerRight/walk 2.png"), this.loadNewImage("src/moving/PlayerRight/walk 3.png"), this.loadNewImage("src/moving/PlayerRight/walk 4.png"), this.loadNewImage("src/moving/PlayerRight/walk 5.png"), this.loadNewImage("src/moving/PlayerRight/walk 6.png"), this.loadNewImage("src/moving/PlayerRight/walk 7.png")];
         this.leftArray = [this.loadNewImage("src/moving/PlayerLeft/walk 1.png"), this.loadNewImage("src/moving/PlayerLeft/walk 2.png"), this.loadNewImage("src/moving/PlayerLeft/walk 3.png"), this.loadNewImage("src/moving/PlayerLeft/walk 4.png"), this.loadNewImage("src/moving/PlayerLeft/walk 5.png"), this.loadNewImage("src/moving/PlayerLeft/walk 6.png"), this.loadNewImage("src/moving/PlayerLeft/walk 7.png")];
         this.index = 0;
-        this.gameState = "begin";
+        this.enemy = this.loadNewImage("src/moving/enemy/shoot 2.png");
+    }
+    jump() {
+        if (this.keyboard.isKeyDown(32) === true) {
+            this.ypos = this.ypos - 20;
+        }
+        if (this.keyboard.isKeyDown(32) === false) {
+            this.ypos = this.ypos + 20;
+            if (this.ypos > this.canvas.height * 8.6 / 10) {
+                this.ypos = this.canvas.height * 8.6 / 10;
+            }
+        }
+    }
+    moveLeft() {
+        if (this.keyboard.isKeyDown(37) === true) {
+            this.index++;
+            if (this.index === 4) {
+                this.drawing(this.leftArray[0]);
+            }
+            if (this.index === 8) {
+                this.drawing(this.leftArray[1]);
+            }
+            if (this.index === 12) {
+                this.drawing(this.leftArray[2]);
+            }
+            if (this.index === 16) {
+                this.drawing(this.leftArray[3]);
+            }
+            if (this.index === 20) {
+                this.drawing(this.leftArray[4]);
+            }
+            if (this.index === 24) {
+                this.drawing(this.leftArray[5]);
+            }
+            if (this.index === 28) {
+                this.drawing(this.leftArray[6]);
+                this.index = 0;
+            }
+            if (this.xpos === 0) {
+                this.xpos = this.canvas.width;
+            }
+            this.xpos = this.xpos - 4;
+        }
+    }
+    moveRight() {
+        if (this.keyboard.isKeyDown(39) === true) {
+            this.index++;
+            if (this.index === 4) {
+                this.drawing(this.array[0]);
+            }
+            if (this.index === 8) {
+                this.drawing(this.array[1]);
+            }
+            if (this.index === 12) {
+                this.drawing(this.array[2]);
+            }
+            if (this.index === 16) {
+                this.drawing(this.array[3]);
+            }
+            if (this.index === 20) {
+                this.drawing(this.array[4]);
+            }
+            if (this.index === 24) {
+                this.drawing(this.array[5]);
+            }
+            if (this.index === 28) {
+                this.drawing(this.array[6]);
+                this.index = 0;
+            }
+            if (this.xpos === this.canvas.width * 5 / 5) {
+                this.xpos = 0;
+            }
+            this.xpos = this.xpos + 4;
+        }
     }
 }
 class KeyboardListener {
