a35bee84e4276c8affa293a954fab0b9643990d5
koenhamstra <70519997+koenhamstra@users.noreply.github.com>
2021-01-04 11:10:59 +0100

    update

--- first-project-Amjad/src/app.ts
+++
@@ -1,9 +0,0 @@
-// Initialize the game after the DOM is loaded.
-let init = () => {
-  const KiwiWars = new Game(
-    document.getElementById("canvas") as HTMLCanvasElement
-  );
-};
-
-// Add EventListener to load the game whenever the browser is ready
-window.addEventListener("load", init);
