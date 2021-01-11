14cb32aeaa853b907b62b33b87a06e91251d2274
amjadhwidy <70522055+amjadhwidy@users.noreply.github.com>
2021-01-11 12:49:50 +0100

    Update Compleet-game.ts

--- src/Compleet-game.ts
+++ src/Compleet-game.ts
@@ -9,11 +9,13 @@ class CompleetGame{
         public constructor(canvas:HTMLCanvasElement){
         this.canvas = canvas;
         this.ctx = this.canvas.getContext("2d");
-        this.classLoader=[new Go (canvas), new Begin (canvas),new Start (canvas)];
+        this.classLoader=[new Go (canvas), new Begin (canvas),new Start(canvas),new FullMarioGame(canvas)];
         this.canvas.width = window.innerWidth;
         this.canvas.height = window.innerHeight;
-        this.classLoader.push(new Go(canvas));
         this.level = 0 ;
+        // this.classLoader[this.level]
+
+
 
         this.loop();
     
@@ -25,9 +27,10 @@ class CompleetGame{
         */
        private loop = () => {
         if (this.classLoader[this.level].done()===true ) {
-            console.log("ásdsd")
-            this.level++;
-            this.classLoader[this.level].loop();
+            console.log(this.level)
+            this.classLoader.splice(0,1);
+            // this.level++
+        
         }
         requestAnimationFrame(this.loop);
         };
